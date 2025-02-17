870445dc0cf9d79fb4f0cd5659e1cea302622289
hixvishnu <hixvishnu@gmail.com>
2025-02-17 17:12:11 +0530

    Comment out the about us page which is created on the home page, we will use it when we will learn react routers

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
 
 function App() {
   return (
@@ -10,8 +10,8 @@ function App() {
     <Navbar title="TextApp" aboutText="About"/>
 
     <div className="container my-3">
-    {/* <TextForm heading="Enter the text to analyze below" /> */}
-    <About />
+    <TextForm heading="Enter the text to analyze below" />
+    {/* <About /> */}
     </div>
 
     </>
