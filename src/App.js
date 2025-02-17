cc128f62e1adee816bd3c35e86cb4af41c558c82
hixvishnu <hixvishnu@gmail.com>
2025-02-17 17:10:41 +0530

    Created an About Us page and used Dark mode concepts Lec - 10

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
-    <Navbar title="TextApp" aboutText="About Uss"/>
+    <Navbar title="TextApp" aboutText="About"/>
 
     <div className="container my-3">
-    <TextForm heading="Enter the text to analyze below" />
+    {/* <TextForm heading="Enter the text to analyze below" /> */}
+    <About />
     </div>
 
     </>
