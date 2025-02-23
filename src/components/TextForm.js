d93ed56c9cf8525c02cb62fd9d5bde9fe1368901
hixvishnu <hixvishnu@gmail.com>
2025-02-23 12:28:39 +0530

    Copy button selected text highliting change

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -17,6 +17,7 @@ export default function TextForm(props) {
         var text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
     }
 
     return (
